e6084d045d85aec04d9757b8af98c260a4c31d54
maher <maherbachouel@gmail.com>
2018-07-19 11:04:43 +0100

    Init angular cli : add component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  //template: '<p>hello</p>',
   styleUrls: ['./app.component.scss']
+  //styles: ['']
 })
 export class AppComponent {
   title = 'app';
